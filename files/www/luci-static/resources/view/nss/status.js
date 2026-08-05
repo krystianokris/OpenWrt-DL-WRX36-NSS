e923ec54228d79a9db78ddaadaacb8350b765624
krystianokris <102819531+krystianokris@users.noreply.github.com>
2026-08-05 06:07:56 +0100

    Improve NSS status view

--- files/www/luci-static/resources/view/nss/status.js
+++ files/www/luci-static/resources/view/nss/status.js
@@ -3,37 +3,22 @@
 'require view';
 'require rpc';
 
-var callNss = rpc.declare({
- object: 'luci.nss-status',
- method: 'read',
- expect: {}
-});
+var callNss = rpc.declare({ object: 'luci.nss-status', method: 'read', expect: {} });
 
 return view.extend({
-
- load: function() {
-  return callNss();
- },
-
- render: function(data) {
-
-  var table = E('table',{
-   'class':'table'
-  });
-
-  for (var k in data) {
-   table.appendChild(
-    E('tr',{},[
-     E('td',{},k),
-     E('td',{},data[k])
-    ])
-   );
-  }
-
-  return E('div',{},[
-   E('h2',{},'NSS Status'),
-   table
-  ]);
- }
-
+	load: function() { return callNss(); },
+	render: function(data) {
+		var rows = [
+			[ 'ath11k NSS offload', data.ath11k_nss ],
+			[ 'ECM module', data.ecm_module ],
+			[ 'NSS module count', data.nss_module_count ],
+			[ 'Loaded NSS modules', data.loaded_modules ]
+		];
+		return E('div', {}, [
+			E('h2', {}, 'NSS Status'),
+			E('table', { 'class': 'table' }, rows.map(function(row) {
+				return E('tr', {}, [ E('td', {}, row[0]), E('td', {}, String(row[1] || 'Unavailable')) ]);
+			}))
+		]);
+	}
 });
