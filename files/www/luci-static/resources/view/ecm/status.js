b3cd2090296c90fe0bf6da67391cc8eabc05da6d
krystianokris <102819531+krystianokris@users.noreply.github.com>
2026-08-05 06:08:00 +0100

    Improve ECM status view

--- files/www/luci-static/resources/view/ecm/status.js
+++ files/www/luci-static/resources/view/ecm/status.js
@@ -3,40 +3,23 @@
 'require view';
 'require rpc';
 
-var callECM = rpc.declare({
- object:'luci.ecm-status',
- method:'read',
- expect:{}
-});
+var callEcm = rpc.declare({ object: 'luci.ecm-status', method: 'read', expect: {} });
 
 return view.extend({
-
-load:function(){
- return callECM();
-},
-
-render:function(data){
-
-var table=E('table',{
-'class':'table'
-});
-
-for(var k in data){
-
-table.appendChild(
- E('tr',{},[
-  E('td',{},k),
-  E('td',{},data[k])
- ])
-);
-
-}
-
-return E('div',{},[
- E('h2',{},'ECM Status'),
- table
-]);
-
-}
-
+	load: function() { return callEcm(); },
+	render: function(data) {
+		var rows = [
+			[ 'IPv4 accelerated', data.ipv4_accelerated ],
+			[ 'TCP accelerated', data.ipv4_tcp ],
+			[ 'UDP accelerated', data.ipv4_udp ],
+			[ 'Total accelerated', data.total_accelerated ],
+			[ 'Pending acceleration', data.pending_accel ]
+		];
+		return E('div', {}, [
+			E('h2', {}, 'ECM Status'),
+			E('table', { 'class': 'table' }, rows.map(function(row) {
+				return E('tr', {}, [ E('td', {}, row[0]), E('td', {}, String(row[1] || '0')) ]);
+			}))
+		]);
+	}
 });
